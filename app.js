148a369ea6c91a28e0f08b95c89a7088525a8fe9
caiog19 <caio@ejcm.com.br>
2022-11-01 21:28:32 -0300

    bug fix

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 3000;
+
 
 app.use(express.static(__dirname));
 app.use(express.static("public"));
 
     
-app.listen(PORT, () => {
-    console.log(`Este app está escutando na port ${PORT}`);
-});
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  });
