a5aece86d1291ecb8b36ab2baca5284bb38f5852
caiog19 <caio@ejcm.com.br>
2022-11-01 21:18:49 -0300

    enviando o resto

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(__dirname));
+app.use(express.static("public"));
+
+    
+app.listen(PORT, () => {
+    console.log(`Este app está escutando na port ${PORT}`);
+});
