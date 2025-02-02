74daf9e381edaa6ebce0b6eef1a0a9d8b0c0725d
Caio Gullo <caioinacio19@gmail.com>
2025-02-02 07:47:56 -0300

    fix: nav-menu active

--- script.js
+++ script.js
@@ -65,7 +65,6 @@ const textsToTranslate = {
     'pt-BR': 'Projeto de dashboard interativo para o Disney+ desenvolvido utilizando Streamlit, que permite a visualização dinâmica de dados e insights sobre o serviço.',
     en: 'Interactive dashboard project for Disney+ developed using Streamlit, which allows dynamic visualization of data and insights about the service.'
   },
-
   'project-spotify-title': {
     'pt-BR': 'Dashboard Interativo Spotify',
     en: 'Interactive Dashboard Spotify'
@@ -74,7 +73,6 @@ const textsToTranslate = {
     'pt-BR': 'Projeto de dashboard interativo para o Spotify desenvolvido com Streamlit, que exibe estatísticas e insights relevantes sobre a plataforma.',
     en: 'Interactive dashboard project for Spotify developed with Streamlit, displaying relevant statistics and insights about the platform.'
   },
-
   'project-navegadev-title': {
     'pt-BR': 'Navega dev',
     en: 'Navega dev'
@@ -83,7 +81,6 @@ const textsToTranslate = {
     "pt-BR": "Este projeto foi desenvolvido com foco na robustez e segurança do backend. Para isso, escolhi o Ruby on Rails como framework principal, o que permitiu uma estrutura organizada e produtiva para construir APIs RESTful. Para o banco de dados, optei pelo PostgreSQL, que oferece alto desempenho e recursos avançados para o armazenamento de dados.\n\nEm termos de segurança, utilizei JWT (JSON Web Tokens) para autenticação e autorização, garantindo que apenas usuários autenticados acessem os recursos. Além disso, implementei bcrypt para criptografar senhas, adicionando uma camada extra de proteção.\n\nPara prevenir abusos de requisições, adotei a gem Rack::Attack, que limita o número de chamadas e ajuda a mitigar ataques de força bruta. Também configurei o rack-cors para gerenciar o compartilhamento de recursos entre diferentes domínios.\n\nNo processamento assíncrono, escolhi o Sidekiq, que, junto com o Redis, gerencia filas de tarefas em segundo plano, otimizando a performance da aplicação. Por fim, utilizei o kaminari para facilitar a paginação de grandes volumes de dados, melhorando a experiência de navegação dos usuários.\n\nEm resumo, essa combinação de tecnologias oferece uma base sólida, segura e escalável, proporcionando uma experiência eficiente e confiável para os usuários.",
     "en": "This project was developed with a focus on backend robustness and security. To achieve this, I chose Ruby on Rails as the main framework, which provided an organized and productive structure for building RESTful APIs. For the database, I opted for PostgreSQL, offering high performance and advanced features for data storage.\n\nIn terms of security, I used JWT (JSON Web Tokens) for authentication and authorization, ensuring that only authenticated users can access the resources. Additionally, I implemented bcrypt for password encryption, adding an extra layer of protection.\n\nTo prevent request abuse, I adopted the Rack::Attack gem, which limits the number of calls and helps mitigate brute force attacks. I also configured rack-cors to manage resource sharing between different domains.\n\nFor asynchronous processing, I chose Sidekiq, which, together with Redis, manages background task queues, optimizing the application's performance. Finally, I used kaminari to facilitate the pagination of large data sets, enhancing the user navigation experience.\n\nIn summary, this combination of technologies provides a solid, secure, and scalable foundation, delivering an efficient and reliable experience for users."
   },
-
   'project-dev-title': {
     'pt-BR': 'DevConnected (Em Construção)',
     en: 'DevConnected (Under Construction)'
@@ -92,7 +89,6 @@ const textsToTranslate = {
     'pt-BR': 'Projeto em desenvolvimento utilizando Next.js, Tailwind, Nest.js, PostgreSQL e Prisma, que tem como objetivo conectar desenvolvedores e promover a colaboração.',
     en: 'Project under development using Next.js, Tailwind, Nest.js, PostgreSQL, and Prisma, aimed at connecting developers and promoting collaboration.'
   },
-
   'project-port-title': {
     'pt-BR': 'Meu Portfólio',
     en: 'My Portfolio'
@@ -153,14 +149,28 @@ document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
 
 const hamburgerMenu = document.querySelector('.hamburger-menu');
 const navMenu = document.querySelector('.nav-menu');
-hamburgerMenu?.addEventListener('click', () => {
-  navMenu?.classList.toggle('active');
+
+hamburgerMenu.addEventListener('click', function (event) {
+  event.stopPropagation();
+  navMenu.classList.toggle('active');
 });
+
 document.querySelectorAll('.nav-menu a').forEach((item) => {
   item.addEventListener('click', () => {
-    navMenu?.classList.remove('active');
+    navMenu.classList.remove('active');
   });
 });
+
+function handleClickOutside(event) {
+  if (
+    navMenu.classList.contains('active') &&
+    !navMenu.contains(event.target) &&
+    !hamburgerMenu.contains(event.target)
+  ) {
+    navMenu.classList.remove('active');
+  }
+}
+document.addEventListener('click', handleClickOutside);
 
 (function () {
   if (window.emailjs) {
@@ -223,6 +233,7 @@ if (document.readyState === "loading") {
   createBgSymbols();
 }
 
+// Projects carousel (multiple slides per page)
 document.addEventListener("DOMContentLoaded", function () {
   const carouselWrapper = document.querySelector('.carousel-wrapper');
   const inner = carouselWrapper.querySelector('.carousel-container');
@@ -242,9 +253,7 @@ document.addEventListener("DOMContentLoaded", function () {
         showPage(i);
       });
       dotsContainer.appendChild(dot);
-      console.log('Dot criado', dot);
     }
-    console.log('Total de dots:', dotsContainer.children.length);
   }
 
   function updateDots(pageIndex) {
@@ -326,7 +335,7 @@ document.addEventListener("DOMContentLoaded", function () {
   }
 });
 
-
+// Secondary carousel (project carousel) functionality
 document.addEventListener("DOMContentLoaded", function () {
   const carouselWrapper = document.querySelector('.project-carousel');
   if (!carouselWrapper) return;
